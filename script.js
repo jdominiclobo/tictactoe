162d162ecb9a221c589837a21d436cf532ea2463
jdominiclobo <jdominiclobo@gmail.com>
2022-02-21 21:03:22 +0530

    draw, reset and win handled

--- script.js
+++ script.js
@@ -1,61 +1,59 @@
-// alert("Welcome to TicTacToe");
-
 //Initial turn
 let turn = "X";
 let gameOver = false;
+let checkCount = 0;
 
 // Function to change the turn
 const changeTurn = () => {
   return turn === "X" ? "O" : "X";
 };
 
+// Winning conditions
+let wins = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let tileTexts = document.getElementsByClassName("tileText");
+
 // Function to check for win
 const checkWin = () => {
-  let tileTexts = document.getElementsByClassName("tileText");
-  let wins = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
+  let message = "";
   wins.forEach((e) => {
     if (
       tileTexts[e[0]].innerText === tileTexts[e[1]].innerText &&
       tileTexts[e[2]].innerText === tileTexts[e[1]].innerText &&
       tileTexts[e[0]].innerText !== ""
     ) {
-      document.querySelector(".display").innerText =
-        tileTexts[e[0]].innerText + " won";
+      message = tileTexts[e[0]].innerText + " won";
       gameOver = true;
     }
   });
+  if (gameOver) {
+    document.querySelector(".display").innerText = message;
+    let ans = confirm(`Game over, reset tiles?`);
+    if (ans) {
+      clearTiles();
+    }
+  }
+  if (checkCount === 9) {
+    document.querySelector(".display").innerText = "Draw";
+    let ans = confirm("Game draw, reset tiles?");
+
+    if (ans) {
+      clearTiles();
+    }
+  }
 };
 
-//Game logic
-let tiles = document.getElementsByClassName("tile");
-Array.from(tiles).forEach((element) => {
-  let tileText = element.querySelector(".tileText");
-  element.addEventListener("click", (e) => {
-    if (tileText.innerText === "") {
-      console.log("clickedy clicks");
-      tileText.innerText = turn;
-      turn = changeTurn();
-      checkWin();
-      if (!gameOver) {
-        document.getElementsByClassName("display")[0].innerText =
-          "Player " + turn + "'s turn";
-      }
-    }
-  });
-});
-
-// Handling reset
-reset.addEventListener("click", () => {
+// Clear tiles once won
+const clearTiles = () => {
   let tileTexts = document.querySelectorAll(".tileText");
   Array.from(tileTexts).forEach((ele) => {
     ele.innerText = "";
@@ -64,4 +62,31 @@ reset.addEventListener("click", () => {
   gameOver = false;
   document.getElementsByClassName("display")[0].innerText =
     "Player " + turn + "'s turn";
+};
+
+//Game logic
+let tiles = document.getElementsByClassName("tile");
+
+for (let i = 0; i < tiles.length; i++) {
+  let tileText = tiles[i].querySelector(".tileText");
+  tiles[i].addEventListener("click", (e) => {
+    if (tileText.innerText === "") {
+      tileText.innerText = turn;
+      checkCount++;
+      turn = changeTurn();
+      setTimeout(() => {
+        checkWin();
+      }, 500);
+
+      if (!gameOver) {
+        document.getElementsByClassName("display")[0].innerText =
+          "Player " + turn + "'s turn";
+      }
+    }
+  });
+}
+
+// Handling reset
+reset.addEventListener("click", () => {
+  clearTiles();
 });
